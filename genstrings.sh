69fce3025a55908e034814ecf8aa7e65f0be4b8f
Adam Pritchard <pritchard.adam@gmail.com>
2017-08-30 11:56:56 -0400

    genstrings.sh should create needed directories
    This allows for a first run that doesn't require pre-existing strings files.

--- genstrings.sh
+++ genstrings.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
+# Copyright (c) 2017, Psiphon Inc.
+# All rights reserved.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 set -e
 
 STRINGS_DIR="Endless/en.lproj"
 TEMP_DIR="${STRINGS_DIR}.temp"
 
-rm -rf ${TEMP_DIR}
-mkdir ${TEMP_DIR}
+mkdir -p "${STRINGS_DIR}"
+
+rm -rf "${TEMP_DIR}"
+mkdir -p "${TEMP_DIR}"
 
 # Go through all source files, extracting localizable strings into appropriate
 # `x.strings` files in a temp directory.
